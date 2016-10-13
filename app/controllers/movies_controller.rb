0083e76e1d3f3152fe1ab7c9257b790faed96753
aramlaka <akhil.ramlakan@gmail.com>
2016-10-13 02:13:36 +0000

    finish

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -16,9 +16,27 @@ class MoviesController < ApplicationController
   end 
   
   def create
-    params.require(:movie)
-    params[:movie].permit(:title,:rating,:release_date)
-    # shortcut: params.require(:movie).permit(:title,:rating,:release_date)
-    # rest of code...
+    @movie = Movie.create!(params[:movie])
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  end
+  # in movies_controller.rb
+
+  def edit
+    @movie = Movie.find params[:id]
+  end
+
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update_attributes!(params[:movie])
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  end
+  
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    redirect_to movies_path
   end
 end
